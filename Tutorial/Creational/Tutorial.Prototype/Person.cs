a10757ab65169e6c8c44d453756abcb48dae58d5
hbaykal <hakkibaykal@gmail.com>
2018-03-14 20:50:08 +0300

    Tutorial-Prototype pattern eklendi

---
+++ Tutorial/Creational/Tutorial.Prototype/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Prototype
+{
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Customer: Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
---
+++ Tutorial/Creational/Tutorial.Prototype/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { FirstName = "Hakkı", LastName = "Baykal", City = "Manisa", Id = 45 };
+           
+            //Customer customer2 = customer1;
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Sedat";
+
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+}
