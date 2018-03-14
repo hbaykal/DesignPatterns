f0013ad227b3fa92511548c97347a15a697e8c2a
hbaykal <hakkibaykal@gmail.com>
2018-03-14 20:07:09 +0300

    Tutorial- FactoryMethod pattern eklendi

---
+++ Tutorial/Creational/Tutorial.Creational.FactoryMethod/LoggerFactory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Creational.FactoryMethod
+{
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+    public class LoggerFactory: ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new TextLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new DbLogger();
+        }
+    }
+
+    public class TextLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with TextLogger");
+        }
+    }
+
+    public class DbLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with DbLogger");
+        }
+    }
+
+    public class CustomerManger
+    {
+        private ILoggerFactory _loggerFactory;
+        public CustomerManger(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+
+}
---
+++ Tutorial/Creational/Tutorial.Creational.FactoryMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Creational.FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManger manager = new CustomerManger(new LoggerFactory2());
+            manager.Save();
+            Console.ReadLine();
+        }
+    }
+}
