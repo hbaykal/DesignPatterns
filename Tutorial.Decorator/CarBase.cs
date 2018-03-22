b47ee556a45fc34f0ab22c343c2bf508f1a34d42
hbaykal <hakkibaykal@gmail.com>
2018-03-22 21:33:52 +0300

    Decorator pattern eklendi

---
+++ Tutorial.Decorator/CarBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Decorator
+{
+    public abstract class CarBase
+    {
+        public abstract string Mark { get; set; }
+        public abstract string Model { get; set; }
+        public abstract decimal HirePrice { get; set; }
+    }
+
+    public class PersonalCar : CarBase
+    {
+        public override decimal HirePrice { get; set; }
+        public override string Mark { get; set; }
+        public override string Model { get; set; }
+    }
+
+    public class CorporateCar : CarBase
+    {
+        public override decimal HirePrice { get; set; }
+        public override string Mark { get; set; }
+        public override string Model { get; set; }
+    }
+
+    public abstract class CarDecoratorBase: CarBase
+    {
+        private CarBase _carBase;
+        public CarDecoratorBase(CarBase carBase)
+        {
+            _carBase = carBase;
+        }         
+    }
+
+    public class SpecialOffer : CarDecoratorBase
+    {
+        public decimal DiscountPercentage { get; set; }
+        private readonly CarBase _carBase;
+        public SpecialOffer(CarBase carBase) : base(carBase)
+        {
+            _carBase = carBase;
+        }
+
+        public override decimal HirePrice
+        {
+            get { return _carBase.HirePrice * ((100-DiscountPercentage)/100); }
+            set { }
+        }
+
+        public override string Mark { get; set; }
+
+        public override string Model { get; set; }
+    }
+}
---
+++ Tutorial.Decorator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personalCar = new PersonalCar() { Mark = "Fiat", Model = "Egea", HirePrice = 35000 };
+
+            SpecialOffer specialOffer = new SpecialOffer(personalCar);
+            specialOffer.DiscountPercentage = 10;
+
+            Console.WriteLine("Personal: {0}", personalCar.HirePrice);
+            Console.WriteLine("Special: {0}", specialOffer.HirePrice);
+            Console.ReadLine();
+
+        }
+    }
+}
