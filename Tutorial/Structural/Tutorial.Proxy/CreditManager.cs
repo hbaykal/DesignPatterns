9489d310ba3c6d22acde27ca3e5483531c991045
hbaykal <hakkibaykal@gmail.com>
2018-03-22 20:52:17 +0300

    Proxy Patter eklendi

---
+++ Tutorial/Structural/Tutorial.Proxy/CreditManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tutorial.Proxy
+{
+    public class CreditManager : CreditBase
+    {
+        public override int Calculate()
+        {
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= i;
+                Thread.Sleep(1000);
+            }
+            return result;
+        }
+    }
+
+    public class CreditmanagerProxy : CreditBase
+    {
+        private CreditManager _creditManger;
+        private int _cachedValue;
+        public override int Calculate()
+        {
+            if (_creditManger == null)
+            {
+                _creditManger = new CreditManager();
+                _cachedValue = _creditManger.Calculate();
+            }
+            return _cachedValue;
+        }
+    }
+}
---
+++ Tutorial/Structural/Tutorial.Proxy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditManager manager = new CreditManager();
+            Console.WriteLine(manager.Calculate());
+            Console.WriteLine(manager.Calculate());
+
+            CreditBase manager2 = new CreditmanagerProxy();
+            Console.WriteLine(manager2.Calculate());
+            Console.WriteLine(manager2.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
