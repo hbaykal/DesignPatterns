92ae1ecac6b4b96cce5a0c4b0fc5913ebc1ea017
hbaykal <hakkibaykal@gmail.com>
2018-04-10 21:55:37 +0300

    TemplateMethod pattern added

---
+++ Tutorial/Behavioral/Tutorial.TemplateMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScoringAlgorithm algorithm;
+            Console.WriteLine("Men : ");
+            algorithm = new MenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(8, new TimeSpan(0, 3, 30)));
+
+            Console.WriteLine("Women : ");
+            algorithm = new WomenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(8, new TimeSpan(0, 3, 30)));
+
+            Console.WriteLine("Children : ");
+            algorithm = new ChildrenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(8, new TimeSpan(0, 3, 30)));
+
+            Console.ReadLine();
+        }
+    }
+}
