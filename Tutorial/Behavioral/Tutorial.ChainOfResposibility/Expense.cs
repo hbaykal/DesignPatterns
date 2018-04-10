074086c0ac31004346168afc43717bc8c330ad28
hbaykal <hakkibaykal@gmail.com>
2018-04-10 20:54:01 +0300

    ChainOfResponsibilty pattern added

---
+++ Tutorial/Behavioral/Tutorial.ChainOfResposibility/Expense.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.ChainOfResposibility
+{
+    public class Expense
+    {
+        public string Detail { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public abstract class ExpenseHandlerBase
+    {
+        protected ExpenseHandlerBase Successor;
+        public abstract void HandleExpense(Expense expense);
+        public void SetSuccessor(ExpenseHandlerBase successor )
+        {
+            Successor = successor;
+        }
+    }
+
+    public class Manager : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Price <= 100)
+            {
+                Console.WriteLine("Manager handled expense");
+            }
+            else if(Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    public class VicePresident : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Price > 100 && expense.Price <=1000)
+            {
+                Console.WriteLine("Vice President handled expense");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    public class President : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Price > 1000)
+            {
+                Console.WriteLine("President handled expense");
+            }
+        }
+    }
+}
---
+++ Tutorial/Behavioral/Tutorial.ChainOfResposibility/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.ChainOfResposibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            VicePresident vicePresident = new VicePresident();
+            President president = new President();
+
+            manager.SetSuccessor(vicePresident);
+            vicePresident.SetSuccessor(president);
+
+            Expense expense = new Expense { Detail = "Rent A Car", Price = 1850 };
+            manager.HandleExpense(expense);
+
+            Console.ReadLine();
+
+        }
+    }
+}
